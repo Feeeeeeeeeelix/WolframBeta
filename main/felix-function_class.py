dc5624f09af495103bf1ec2a43d382f5f8c10b34
Feeeeeeeeeelix <90841338+Feeeeeeeeeelix@users.noreply.github.com>
2021-09-26 14:38:06 +0200

    Add files via upload

--- main/felix-function_class.py
+++ main/felix-function_class.py
@@ -1,7 +1,7 @@
 
 
 
-functions = ["exp", "ln", "arccos", "arcsin", "arctan", "sin", "cos", "tan"]
+functions = ["exp", "ln", "arccos", "arcsin", "arctan", "sin", "cos", "tan", "sqrt"]
 alphabet = "qwertzuiopasdfghjklyxcvbnm"
 numbers = "0123456789"
 var = x = "x"
@@ -38,44 +38,59 @@ def replace_arg(f, innerargs):
 			fstr = fstr.replace("@", f"({innerargs[n]})", 1)
 			n += 1
 	return eval(fstr)
+	
+"""
+1. args entfernen
+2. äusseren +/- splitten, einzelnt parsen
+3. äusseren *,/ splitten, einzelnt parsen
 
+EX: sin(sqrt(e^x+3)/2)
+(x+4)(x+6^x)ln(x)^2
+ln(cos(x^4))
+-cos(x)*x^3
+-sqrt(3*x)+3
+"""
 
 def parse(f):
 	f = f.replace(" ", "")
 	
-	# ~ print(f"parse:{f}")			##
+	print(f"parse:{f}")			##
 	
 	if f in numbers+var:
-		# ~ print(f"constfound: {f=}")
+		print(f"constfound: {f=}")
 		return f
 	
 	f, innerargs = inner_args(f)
 	
-	# ~ print(f"{f=}, {innerargs=}")		##
+	print(f"{f=}, {innerargs=}")		##
 	
 	if "+" in f:
 		summands = replace_arg(f.split("+"), innerargs)	
-		# ~ print(f"{summands=}")				##
+		print(f"{summands=}")				##
 		return ["+", [parse(s) for s in summands]]
 			
 	if "-" in f:
 		subs = replace_arg(f.split("-"), innerargs)	
-		# ~ print(f"{subs=}")				##
+		print(f"{subs=}")
+		if len(subs) == 2 and subs[0] == "":
+			return ["*", [-1, parse(subs[1])]]
+			
+						##
 		return ["-", [parse(s) for s in subs]]
 			
 	if "*" in f:		
 		factors = replace_arg(f.split("*"), innerargs)
-		# ~ print(f"{factors=}")			##
+		print(f"{factors=}")			##
 		return ["*", [parse(s) for s in factors]]
 		
 	if "/" in f:		
 		div = replace_arg(f.split("/"), innerargs)
-		# ~ print(f"{div=}")			##
+		print(f"{div=}")			##
 		return ["/", [parse(s) for s in div]]
 				
 	if "^" in f:
 		base, exp = replace_arg(f.split("^"), innerargs)
-		# ~ print(f"{base=}, {exp=}")			##
+		print(f"{base=}, {exp=}")			##
 		return ["^", [parse(base), parse(exp)]]
 	
 	if f[0] in "sctela":
@@ -85,6 +100,42 @@ def parse(f):
 	
 	if f == "@":
 		return parse(innerargs[0])
+		
+"""
+['+', [['*', [-1, ['sqrt', ['*', ['3', 'x']]]]], '3']]  
+
+
+['+',[
+	['*',[
+		-1, 
+		['+', [
+			['*', [
+				['*', [
+					['*', [
+						'3', 
+						['+', [
+							['*', 
+								[1]
+							]
+							]]
+						],
+						['/', [1, ['*', [2, ['sqrt', ['*', ['3', 'x']]]]]]]
+						]]]]]]]]]] 
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def write(f):
@@ -103,7 +154,7 @@ def write(f):
 				while "1" in args:
 					args.remove("1")
 			
-			return {"+":" + ", "-":" - ", "*":" * "}[f[0]].join(args)
+			return {"+":" + ", "-":" - ", "*":"*"}[f[0]].join(args)
 				
 		if f[0] == "/":
 			num = f"({write(f[1][0])})" if type(f[1][0]) == list else f[1][0]
@@ -113,6 +164,10 @@ def write(f):
 		if f[0] == "^":
 			base = f"({write(f[1][0])})" if type(f[1][0]) == list and f[1][0][0] not in functions else write(f[1][0])
 			power = f"({write(f[1][1])})" if type(f[1][1]) == list else f[1][1]
+			try:
+				power = eval(power)
+			except:
+				pass
 			return f"{base}^{power}"
 				
 		if f[0] in functions:
@@ -128,13 +183,15 @@ def write(f):
 
 
 def diff(f, var = "x"):
-
+	# ~ print(f"diff: {f=}")
 	def funcderivative(f):
-		
+		# ~ print(f"funcdiff: {f=}")
+
 		
 		dln = lambda u: ["/", [1, u]]
 		dlog = lambda u: ["/", [1, u]]
 		dexp = lambda u: ["exp", u]
+		dsqrt = lambda u: ["/", [1, ["*", [2, ["sqrt", u]]]]]
 		
 		dsin = lambda u: ["cos", u]
 		dcos = lambda u: ["*", [-1, ["sin", u]]]
@@ -161,14 +218,21 @@ def diff(f, var = "x"):
 			return ["*", [du, eval("d"+f[0])(u)]]
 	
 	def isconst(a):
-		return True if (str(a).isalnum() or (a[0] == "-" and a[1:].isalnum())) and var not in str(a) else False
+		return not var in str(a)
 	
 	if type(f) == list:
 		if f[0] == "*": #2*4*sinx*x
 			constfactors = [factor for factor in f[1] if isconst(factor)]
 			funcfactors = [factor for factor in f[1] if factor not in constfactors]
 			
-			return ["*", [*constfactors, ["+", [["*", [diff(f), *[g for g in funcfactors if g != f]]] for f in funcfactors]]]]
+			# ~ print(f"{constfactors=}, {funcfactors=}")
+
+
+
+			if len(funcfactors) == 1:
+				return ["*", [*constfactors, diff(funcfactors[0])]]
+			else:
+				return ["*", [*constfactors, ["+", [["*", [diff(f), *[g for g in funcfactors if g != f]]] for f in funcfactors]]]]
 	
 		elif f[0] == "/":
 			if isconst(f[1][0]):							#case (k/u)' = k*(u^-1)'
@@ -198,6 +262,8 @@ def diff(f, var = "x"):
 	return 1 if f == var else 0
 	
 
+
+
 class Function:
 	def __init__(self, inputfunc):
 		if type(inputfunc) == str:
@@ -220,11 +286,23 @@ class Function:
 
 
 func = "(x+4+0)*(x+6^x)*ln(x)^2"
-func = "1*1*2*3*4*arcsin(x)*x"
+func = "-sqrt(3*x)+3"
+func = "-cos(x)*x^3"
 
 f = Function(func)
 baum = f.tree
+
+# ~ print(f"\nf(x) = {func}\n\nTree: {baum}\n\nwrite: {f.str}\n\nf'(x): {diffed.tree}\n\nf'(x) = {diffed.str}")
+
+print()
+print(f"{func = }\n")
+print(f"{baum = }\n")
+print(f"{f.str = }\n")
 diffed = f.diff()
 
-print(f"\nf(x) = {func}\n\nTree: {baum}\n\nwrite: {f.str}\n\nf'(x): {diffed.tree}\n\nf'(x) = {diffed.str}")
+print(f"{diffed.tree = }\n")
+print(f"{diffed.str = }\n")
 
+# ~ p = parse(diffed.str)
+# ~ print(f"{p = }")
+# ~ print(f"{write(p) = }")
