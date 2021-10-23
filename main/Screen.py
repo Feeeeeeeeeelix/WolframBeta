e87f9848b9afc605fabebe50d20664e3e8eb324a
Feeeeeeeeeelix <90841338+Feeeeeeeeeelix@users.noreply.github.com>
2021-10-23 22:26:09 +0200

    Update Screen.py

--- main/Screen.py
+++ main/Screen.py
@@ -9,12 +9,13 @@ from tkinter import *
 
 
 
-def show():
+def calculate():
 	text = inputentry.get()
 	text = r"$"+text+"$"
-	wx.clear()
-	wx.text(0.1, 0.5, text, fontsize = 8)
-	canvas.draw()
+	outlabel["text"] = text
+	# wx.clear()
+	# wx.text(0.1, 0.5, text, fontsize = 8)
+	# canvas.draw()
 
 
 root = Tk()
@@ -28,6 +29,8 @@ sh = 500
 root.geometry(f"{sw}x{sh}")
 root.title("Wolframbeta")
 
+lblue = "#1e3799"
+dblue = "#001B81"
 
 
 	# Topframe
@@ -42,7 +45,7 @@ logo.place(relx = 0.45, rely = 0.35)
 
 
 	# Leftframe
-leftframe = Frame(root, bg="#1e3799")
+leftframe = Frame(root, bg = lblue)
 leftframe.place(y = 0, x = 0, relheight = 1, relwidth = 0.1)
 
 whitebg = Label(leftframe)
@@ -51,14 +54,14 @@ whitebg.place(rely = 0.01, relx = 0.05, relwidth = 0.9, relheight = 0.29)
 sep = Label(leftframe, bg = "white")
 sep.place(x = 0, rely = 0.299, relwidth = 1, height = 2)
 
-selectframe = Frame(leftframe, bg="#1e3799")
-selectframe.place(x = 0, rely = 0.301, relwidth = 1, relheight = 0.69)
+selectframe = Frame(leftframe, bg = lblue)
+selectframe.place(x = 0, rely = 0.301, relwidth = 1, relheight = 0.69) #(nice)
 
 
 def SelectionButtons(container, function, *names):
 	buttons = []
 	for i, name in enumerate(names):
-		button = Button(container, text = name, bg = "#1e3799", fg = "white", highlightthickness = 0, bd = 0, activebackground="#0c2461", activeforeground="white", command = lambda n=i+1: eval(function)(n))
+		button = Button(container, text = name, bg = lblue, fg = "white", highlightthickness = 0, bd = 0, activebackground = dblue, activeforeground = "white", command = lambda n=i+1: eval(function)(n))
 		buttons.append(button)
 		button.place(rely = i/10, x = 0, relwidth = 1, relheight = 0.0985)
 	return buttons
@@ -74,16 +77,16 @@ def select(topic):
 	
 	clear_frame()
 	for i in range(3):
-		buttons[i]["bg"] = "#1e3799"
+		buttons[i]["bg"] = lblue
 		
 	if selection == 1:
-		buttons[0]["bg"] = "#0c2461"
+		buttons[0]["bg"] = dblue
 		SelectionButtons(selectframe, "analysis", "Ableitung", "Integral", "Grenzwert", "Graph")
 	if selection == 2:
-		buttons[1]["bg"] = "#0c2461"
+		buttons[1]["bg"] = dblue
 		SelectionButtons(selectframe, "algebra", "Matrizen")
 	if selection == 3:
-		buttons[2]["bg"] = "#0c2461"
+		buttons[2]["bg"] = dblue
 		SelectionButtons(selectframe, "numbers", "Zahlentheorie")
 
 
@@ -100,7 +103,6 @@ def numbers(n):
 
 	
 buttons = SelectionButtons(leftframe, "select", "Analysis", "Algebra", "Numbers")
-print(buttons)
 
 
 	# Mittleframe
@@ -110,19 +112,22 @@ mittleframe.place(rely = 0.1, relx = 0.1, relheight = 0.8, relwidth=0.9)
 
 
 
-inputframe = Frame(mittleframe, highlightbackground="#1e3799", highlightcolor="green", highlightthickness=2)
+inputframe = Frame(mittleframe, highlightbackground = lblue, highlightcolor = "green", highlightthickness=2, bg = "white")
 inputframe.place(relx = 0.1, rely = 0.2, relwidth = 0.3, relheight = 0.6)
 
 inputentry = Entry(inputframe, bd=0, highlightthickness=0)
-inputentry.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+inputentry.place(relx = 0.05, rely = 0.05, relwidth = 0.9, relheight = 0.3)
 inputentry.insert(0, "\sin(x) = \sum_{n=0}^\infty (-1)^n\cdot \\frac{x^{2n+1}}{(2n+1)!}")
+inputentry.bind("<Return>", calculate)
 
-
-bttn = Button(mittleframe, text = "=", command = show)
+bttn = Button(mittleframe, text = "=", command = calculate)
 bttn.place(relx = 0.45, rely = 0.45, relwidth = 0.1, relheight=0.1)
 
-outputframe = Frame(mittleframe, highlightbackground = "#1e3799", highlightthickness = 3)
+outputframe = Frame(mittleframe, highlightbackground = lblue, highlightthickness = 2)
 outputframe.place(relx = 0.6, rely = 0.2, relwidth = 0.3, relheight=0.6)
+
+outlabel = Label(outputframe)
+outlabel.pack(expand=1, fill="both")
 
 # fig = matplotlib.figure.Figure(figsize=(1,1), dpi=200)
 # wx = fig.add_subplot(111)
