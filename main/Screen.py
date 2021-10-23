cb9af964c3297d1a5b793bf2962facb1f409c40d
Felix <felix.dehon@gmail.com>
2021-10-23 16:50:01 +0200

    Updated Screen.py

--- main/Screen.py
+++ main/Screen.py
@@ -1,11 +1,11 @@
 from tkinter import *
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# import matplotlib
+# import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-matplotlib.use('TkAgg')
-plt.rcParams["mathtext.fontset"] = "cm"
+# matplotlib.use('TkAgg')
+# plt.rcParams["mathtext.fontset"] = "cm"
 
 
 
@@ -36,18 +36,56 @@ logo = Label(topframe, image=logopic)
 logo.place(relx = 0.45, rely = 0.35)
 
 
+
+
 	# Leftframe
-leftframe = Frame(root, borderwidth=3, bg="#1e3799")
+leftframe = Frame(root, bg="#1e3799")
 leftframe.place(y = 0, x = 0, relheight = 1, relwidth = 0.1)
 
-bttn1 = Button(leftframe, text = "Analysis", bg = "#1e3799", fg = "white", highlightthickness = 0, highlightbackground = "#1e3799")
-bttn1.place(rely = 0.001, x = 0, relwidth = 1, relheight = 0.09)
+whitebg = Label(leftframe)
+whitebg.place(rely = 0.01, relx = 0.05, relwidth = 0.9, relheight = 0.29)
 
-bttn2 = Button(leftframe, text = "Algebra", bg = "#1e3799", fg = "white", highlightthickness = 0)
-bttn2.place(rely = 0.101, x = 0, relwidth = 1, relheight = 0.09)
+class SelectionButtons:
+	def __init__(self, container, function, *names):
+		print(names)
+		for i, name in enumerate(names):
+			button = Button(container, text = name, bg = "#1e3799", fg = "white", highlightthickness = 0, bd = 0, activebackground="#0c2461", activeforeground="white", command = lambda: eval(f"{function}({i+1})"))
+			button.place(rely = i/10, x = 0, relwidth = 1, relheight = 0.099)
 
-bttn3 = Button(leftframe, text = "Numbers", bg = "#1e3799", fg = "white")
-bttn3.place(rely = 0.201, x = 0, relwidth = 1, relheight = 0.09)
+
+
+bttn = SelectionButtons(leftframe, "select", "Analysis", "Algebra", "Numbers")
+
+# bttn1 = Button(leftframe, text = "Analysis", bg = "#1e3799", fg = "white", highlightthickness = 0, bd = 0, activebackground="#0c2461", activeforeground="white", command = lambda: select(1))
+# bttn1.place(rely = 0, x = 0, relwidth = 1, relheight = 0.099)
+
+# bttn2 = Button(leftframe, text = "Algebra", bg = "#1e3799", fg = "white", highlightthickness = 0, bd = 0, activebackground="#0c2461", activeforeground="white", command = lambda: select(2))
+# bttn2.place(rely = 0.1, x = 0, relwidth = 1, relheight = 0.099)
+
+# bttn3 = Button(leftframe, text = "Numbers", bg = "#1e3799", fg = "white", highlightthickness = 0, bd = 0, activebackground="#0c2461", activeforeground="white", command = lambda: select(3))
+# bttn3.place(rely = 0.2, x = 0, relwidth = 1, relheight = 0.099)
+
+selectframe = Frame(leftframe)
+selectframe.place(x = 0, rely = 0.301, relwidth = 1, relheight = 0.69)
+
+selection = 0
+def select(topic):
+	global selection
+	selection = topic if selection != topic else 0
+	eval(["emptysel", "analysis", "algebra", "numbers"][selection])()
+	
+def analysis():
+	pass
+	
+def algebra():
+	print("algebra")
+	
+def numbers():
+	print("numbers")
+
+def emptysel():
+	print("empty")
+
 
 
 	# Mittleframe
@@ -57,32 +95,30 @@ mittleframe.place(rely = 0.1, relx = 0.1, relheight = 0.8, relwidth=0.9)
 
 
 
-inputframe = Frame(mittleframe, highlightbackground = "#1e3799", highlightthickness = 3)
+inputframe = Frame(mittleframe, highlightbackground="#1e3799", highlightcolor="green", highlightthickness=2)
 inputframe.place(relx = 0.1, rely = 0.2, relwidth = 0.3, relheight = 0.6)
 
-inputentry = Entry(inputframe)
+inputentry = Entry(inputframe, bd=0, highlightthickness=0)
 inputentry.place(x = 0, y = 0, relwidth = 1, relheight = 1)
 inputentry.insert(0, "\sin(x) = \sum_{n=0}^\infty (-1)^n\cdot \\frac{x^{2n+1}}{(2n+1)!}")
 
 
-equal_bttn = Frame(mittleframe)#, highlightbackground = "#1e3799", highlightthickness = 3)
-equal_bttn.place(relx = 0.45, rely = 0.45, relwidth = 0.1, relheight=0.1)
-bttn = Button(equal_bttn, text = "=", command = show, highlightbackground = "#1e3799", highlightthickness = 3)
-bttn.pack(expand=1, fill="both")
+bttn = Button(mittleframe, text = "=", command = show)
+bttn.place(relx = 0.45, rely = 0.45, relwidth = 0.1, relheight=0.1)
 
 outputframe = Frame(mittleframe, highlightbackground = "#1e3799", highlightthickness = 3)
 outputframe.place(relx = 0.6, rely = 0.2, relwidth = 0.3, relheight=0.6)
 
-fig = matplotlib.figure.Figure(figsize=(1,1), dpi=200)
-wx = fig.add_subplot(111)
-canvas = FigureCanvasTkAgg(fig, master=outputframe)
-canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=0)
-canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
-wx.get_xaxis().set_visible(False)
-wx.get_yaxis().set_visible(False)
+# fig = matplotlib.figure.Figure(figsize=(1,1), dpi=200)
+# wx = fig.add_subplot(111)
+# canvas = FigureCanvasTkAgg(fig, master=outputframe)
+# canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=0)
+# canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
+# wx.get_xaxis().set_visible(False)
+# wx.get_yaxis().set_visible(False)
 
 
-show()
+# show()
 
 	# Bottomframe
 bottomframe = Label(root, borderwidth=3, relief="raised")
